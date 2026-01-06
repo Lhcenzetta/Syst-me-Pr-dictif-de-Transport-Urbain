5d7eabe969059398a3189d098b8c1410406af992
lait-zet <lhcenztta18@gmail.com>
2026-01-06 14:37:14 +0100

    get data ready for mlgit add .git add .

---
+++ main.py
@@ -0,0 +1,9 @@
+# import pandas as pd
+
+# # Lecture du fichier
+# df = pd.read_parquet('data/bronze_taxi.parquet')
+
+# # Vérification
+# print(df.head())
+# print(df.info())
+from pyspark.sql import SparkSession
